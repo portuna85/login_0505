f7e2f3b98a021e3dd2de7391b8a157d7accbbbba
portuna85 <portuna85@gmail.com>
2022-03-23 09:30:42 +0900

    Login Cookie, Session

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -82,8 +82,6 @@ public class HomeController {
     @GetMapping("/")
     public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
 
-
-
         // 세션에 회원 데이터가 없으면 home
         if (loginMember == null) {
             return "home";
